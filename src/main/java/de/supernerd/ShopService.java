bc0d4c038e3589341e5ae81aedb884b2f07b2fc9
Manuel Simon <info@der-supernerd.de>
2025-02-17 21:16:07 +0100

    Initial commit

---
+++ src/main/java/de/supernerd/ShopService.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ShopService {
+    private ProductRepo productRepo = new ProductRepo();
+    private OrderRepo orderRepo = new OrderMapRepo();
+
+    public Order addOrder(List<String> productIds) {
+        List<Product> products = new ArrayList<>();
+        for (String productId : productIds) {
+            Product productToOrder = productRepo.getProductById(productId);
+            if (productToOrder == null) {
+                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                return null;
+            }
+            products.add(productToOrder);
+        }
+
+        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+
+        return orderRepo.addOrder(newOrder);
+    }
+}
